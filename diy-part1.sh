cea40916c51130b8790c03b0a1f0e5953aa65c63
Hokia <58064663+Hokia@users.noreply.github.com>
2023-09-11 13:37:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall0 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git passwall_dep0 https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git helloworld0 https://github.com/fw876/helloworld' >>feeds.conf.default
-
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
